ec6332245efe57a6a0aefff0a21773816325f4c2
Juan Tapia <jsteventapia10@live.com>
2020-11-10 17:18:55 -0500

    Create Model and html

--- voting/models.py
+++ voting/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Post(models.Model):
+    header = models.CharField(max_length=50)
+    body = models.TextField(max_length=250, default='')
+    # post_submitter_name = models.CharField(max_length=100)  # name os student who posted post
+    # post_submitter_username = models.CharField(max_length=10)  # username of person who submitted Post
+    date_published = models.DateTimeField(default=datetime.now, blank=True)
+
+    @classmethod
+    def create(cls, header, body, post_submitter_username):
+        post = cls(header=header, body=body, post_submitter_username=post_submitter_username)
+        return post
+
+    def __str__(self):
+        return self.header
+
--- voting/views.py
+++ voting/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+
+def dashboard(request):
+    posts = Post.objects.all()
+
+    return render(request, 'voting/dashboard.html', {'posts': posts, })
+
