c7d2d39268f5a978e5abab2037bd1d18aa9b1c04
Juan Tapia <jsteventapia10@live.com>
2020-11-12 22:31:00 -0500

    Add Create Post

--- advocator/urls.py
+++ advocator/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from voting import views
+
+app_name = "advocator"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.dashboard, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('posts/create/', views.CreatePostView.as_view(), name='createPost'),
 
 ]
--- voting/views.py
+++ voting/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from .models import *
+from django.views import generic
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import redirect
 
 
 def dashboard(request):
     posts = Post.objects.all()
 
     return render(request, 'voting/dashboard.html', {'posts': posts, })
+
+
+class CreatePostView(generic.CreateView):
+    model = Post
+    fields = ['header', 'body']
+    template_name = 'voting/createPost.html'
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.save()
+        return HttpResponseRedirect(reverse('advocator:dashboard', None))
+
